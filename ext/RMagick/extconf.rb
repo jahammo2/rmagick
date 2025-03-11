6f9d466d558ecfc03964357190ef77b7915643a4
Jordan <jahammo2@gmail.com>
2025-03-11 10:00:05 -0600

    WIP

--- ext/RMagick/extconf.rb
+++ ext/RMagick/extconf.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+abort("extconf.rb is running!")
+
 lib_dir = File.expand_path('../../lib', File.dirname(__FILE__))
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 require 'mkmf'
